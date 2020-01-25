c49777aa82ae277a19186fb921c8d60c44c4172e
albertgoncalves <goncalves.albert@gmail.com>
2020-01-25 15:01:01 -0500

    ~2700% faster

--- dev/src/receive/sanitize.rs
+++ dev/src/receive/sanitize.rs
@@ -1,28 +1,60 @@
 /* NOTE: https://api.slack.com/docs/message-formatting */
 pub fn sanitize(input: &str) -> String {
-    let chars: Vec<char> = input
-        .replace("&amp;", "&")
-        .replace("&lt;", "")
-        .replace("&gt;", "")
-        .replace("\\u201c", "\"")
-        .replace("\\u201d", "\"")
-        .replace("\\u2018", "\"")
-        .replace("\\u2019", "\"")
-        .chars()
-        .collect();
+    let chars: &[u8] = input.as_bytes();
     let n: usize = chars.len();
     let mut output: String = String::with_capacity(n);
-    match chars[0] {
-        '\\' => (),
-        '\n' => output.push(' '),
-        c => output.push(c),
-    }
-    for i in 0..(n - 1) {
-        match (chars[i], chars[i + 1]) {
-            ('\\', 'n') | (_, '\n') => output.push(' '),
-            (_, '\\') | (_, '*') | (_, '_') | (_, '~') | (_, '`') => (),
-            (_, c) => output.push(c),
+    let mut i = 0;
+    loop {
+        if n <= i {
+            break;
         }
+        match chars[i] as char {
+            '\n' => output.push(' '),
+            '\\' => {
+                if ((i + 1) < n) && (chars[i + 1] == b'n') {
+                    output.push(' ');
+                    i += 2;
+                    continue;
+                }
+                if ((i + 5) < n)
+                    && (chars[i + 1] == b'u')
+                    && (chars[i + 2] == b'2')
+                    && (chars[i + 3] == b'0')
+                    && (chars[i + 4] == b'1')
+                    && ((chars[i + 5] == b'c')
+                        || (chars[i + 5] == b'd')
+                        || (chars[i + 5] == b'8')
+                        || (chars[i + 5] == b'9'))
+                {
+                    output.push('\"');
+                    i += 6;
+                    continue;
+                }
+            }
+            '&' => {
+                if ((i + 3) < n)
+                    && ((chars[i + 1] == b'l') || (chars[i + 1] == b'g'))
+                    && (chars[i + 2] == b't')
+                    && (chars[i + 3] == b';')
+                {
+                    i += 4;
+                    continue;
+                }
+                if ((i + 4) < n)
+                    && (chars[i + 1] == b'a')
+                    && (chars[i + 2] == b'm')
+                    && (chars[i + 3] == b'p')
+                    && (chars[i + 4] == b';')
+                {
+                    output.push('&');
+                    i += 5;
+                    continue;
+                }
+            }
+            '*' | '_' | '~' | '`' => (),
+            c => output.push(c),
+        }
+        i += 1;
     }
     output
 }
