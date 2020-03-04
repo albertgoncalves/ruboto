b3436f898508f37b029277ae7fbca12ca4deb92f
albertgoncalves <goncalves.albert@gmail.com>
2020-03-04 17:59:32 -0500

    pin openssl and updated rust-ws

--- shell.nix
+++ shell.nix
@@ -1,8 +1,20 @@
-with import <nixpkgs> {};
+# $ cat ~/.nix-defexpr/channels/nixpkgs/.git-revision
+# REVISION
+# $ nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/REVISION.tar.gz
+# SHA256
+
+let
+    nixpkgs = (import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/cc1ae9f21b9e0ce998e706a3de1bad0b5259f22d.tar.gz";
+        sha256 = "0zjafww05h50ncapw51b5qxgbv9prjyag0j22jnfc3kcs5xr4ap0";
+    })) {};
+in
+
+with nixpkgs;
 let
     shared = [
         jq
-        openssl_1_0_2
+        openssl
         rustup
         shellcheck
     ];
